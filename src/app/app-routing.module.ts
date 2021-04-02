256f60a8a4c25737337c2d82c2d5457a6dfcece1
eugene.sky <skytech4dotnet@gmail.com>
2021-04-02 18:22:31 -0500

    ng g @angular/material:material-nav --name=main-nav; truck module; routing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TruckListComponent } from './truck/truck-list/truck-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'truck-list', component: TruckListComponent },
+  { path: '', redirectTo: 'truck-list', pathMatch: 'full' },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/truck/truck.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TruckRoutingModule } from './truck-routing.module';
+import { TruckListComponent } from './truck-list/truck-list.component';
+
+
+@NgModule({
+  declarations: [TruckListComponent],
+  imports: [
+    CommonModule,
+    TruckRoutingModule
+  ]
+})
+export class TruckModule { }
