df93361f6e33bb17e1928c8f0313bf066e9947f6
eugene.sky <skytech4dotnet@gmail.com>
2021-04-03 16:49:35 -0500

    ng g @angular/material:material-table --name=truck-table

--- src/app/truck/truck.module.ts
+++ src/app/truck/truck.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { TruckRoutingModule } from './truck-routing.module';
 import { TruckListComponent } from './truck-list/truck-list.component';
+import { TruckTableComponent } from './truck-table/truck-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 
 @NgModule({
-  declarations: [TruckListComponent],
+  declarations: [TruckListComponent, TruckTableComponent],
   imports: [
     CommonModule,
-    TruckRoutingModule
+    TruckRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   exports: [ TruckListComponent ]
 })
