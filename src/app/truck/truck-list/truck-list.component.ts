d1d1531db1ef506f26ff961d53180c08689be72a
eugene.sky <skytech4dotnet@gmail.com>
2021-04-07 11:13:04 -0500

    delete truck

--- src/app/truck/truck-list/truck-list.component.ts
+++ src/app/truck/truck-list/truck-list.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { Truck } from '../truck';
 import { TruckService } from '../truck.service';
 
@@ -8,11 +10,39 @@ import { TruckService } from '../truck.service';
   templateUrl: './truck-list.component.html',
   styleUrls: ['./truck-list.component.scss']
 })
-export class TruckListComponent {
-  displayedColumns: string[] = [ 'truckName', 'dispatch', 'city', 'state', 'when', 'comments' ];
-  trucks: Observable<Truck[]>;
+export class TruckListComponent implements OnInit, OnDestroy {
+  displayedColumns: string[] = [ 'truckName', 'dispatch', 'city', 'state', 'when', 'comments', 'actions' ];
+  trucks: Truck[] = [];
+  dataSource = new MatTableDataSource<Truck>(this.trucks);
+  private ngUnsubsribe = new Subject();
 
-  constructor(private truckService: TruckService) {
-    this.trucks = this.truckService.getTrucks();
+  constructor(private truckService: TruckService) { }
+
+  ngOnInit(): void {
+    this.truckService.getTrucks()
+      .pipe(
+        takeUntil(this.ngUnsubsribe)
+      )
+      .subscribe((trucks: Truck[]) => {
+        this.trucks = trucks;
+        this.dataSource = new MatTableDataSource<Truck>(this.trucks);
+      });
+  }
+
+  deleteTruck(id: number): void {
+    this.truckService.deleteTruck(id)
+      .pipe(
+        takeUntil(this.ngUnsubsribe)
+      )
+      .subscribe(() => {
+        const index: number = this.trucks.findIndex(truck => truck.truckId === id);
+        this.trucks.splice(index, 1);
+        this.dataSource = new MatTableDataSource<Truck>(this.trucks);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubsribe.next();
+    this.ngUnsubsribe.complete();
   }
 }
